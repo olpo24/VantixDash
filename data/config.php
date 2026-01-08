9e9fba7ff5b3112ca4d933f32e7fc236759d072e
Ole Poetter <48469242+olpo24@users.noreply.github.com>
2026-01-08 15:46:59 +0100

    Create config.php

---
+++ data/config.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'username' => 'olpo',
+  'password_hash' => '$2y$10$4gewp4YghDzpZ0pFUyCJCOYEwC/39sFARY8gI8/tJpKZH9pw/lELe',
+  'email' => 'wp@olpo.de',
+  'reset_token' => NULL,
+  'token_expires' => NULL,
+  '2fa_enabled' => true,
+  '2fa_secret' => 'H53BYCO3ASSZ7KKH',
+);
