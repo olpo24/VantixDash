2ba28ff35ae4c5a619e7dea7a6e692542148d71b
olpo24 <olpo24@users.noreply.github.com>
2026-01-09 06:36:35 +0000

    chore: bump version to 1.0 [skip ci]

--- version.php
+++ version.php
@@ -4,5 +4,5 @@
  * Hält die aktuelle Versionsnummer des Dashboards.
  */
 return [
-    'version' => '1.5.1', // Diese Nummer erhöhst du bei jedem Release
+    'version' => '1.0', // Diese Nummer erhöhst du bei jedem Release
 ];
