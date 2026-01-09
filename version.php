8a09d59a089e3c41902b1f7e44bfd72e534c0ad5
Ole Poetter <48469242+olpo24@users.noreply.github.com>
2026-01-09 07:29:44 +0100

    Update version.php

--- version.php
+++ version.php
@@ -1,5 +1,8 @@
 <?php
+/**
+ * version.php
+ * Hält die aktuelle Versionsnummer des Dashboards.
+ */
 return [
-    'version' => '1.6.0',
-    'remote_url' => 'https://raw.githubusercontent.com/olpo24/VantixDash/main/version.php'
+    'version' => '1.5.1', // Diese Nummer erhöhst du bei jedem Release
 ];
