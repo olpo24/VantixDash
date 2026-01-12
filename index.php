c10c8e05542d9ea2fa38e0ec6468c158e78adb34
Ole Poetter <48469242+olpo24@users.noreply.github.com>
2026-01-12 15:30:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -45,6 +45,7 @@ if (!file_exists($viewPath)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VantixDash - v<?php echo $configService->getVersion(); ?></title>
+    <script src="https://unpkg.com/@phosphor-icons/web"></script>
     <link rel="stylesheet" href="assets/css/style.css"> <meta name="csrf-token" content="<?php echo $_SESSION['csrf_token'] ?? ''; ?>">
 </head>
 <body>
