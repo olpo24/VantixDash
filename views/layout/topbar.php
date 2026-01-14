d43f10b108c23adc59ead3eeed4816e724ad6a63
Ole Poetter <48469242+olpo24@users.noreply.github.com>
2026-01-14 15:07:09 +0100

    Update topbar.php

--- views/layout/topbar.php
+++ views/layout/topbar.php
@@ -1,1 +1,8 @@
-
+<header class="topbar">
+    <button id="sidebar-toggle" class="sidebar-toggle">
+        <i class="ph ph-list"></i>
+    </button>
+    <div class="topbar-info">
+        <span>VantixDash v<?php echo htmlspecialchars((string)$configService->getVersion()); ?></span>
+    </div>
+</header>
