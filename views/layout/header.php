ef920ee020dda308e9455ce851f0c50891f5536a
Ole Poetter <48469242+olpo24@users.noreply.github.com>
2026-01-14 15:06:33 +0100

    Update header.php

--- views/layout/header.php
+++ views/layout/header.php
@@ -1,1 +1,11 @@
-
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VantixDash - v<?php echo htmlspecialchars((string)$configService->getVersion()); ?></title>
+    <script src="https://unpkg.com/@phosphor-icons/web"></script>
+    <link rel="stylesheet" href="assets/css/style.css"> 
+    <meta name="csrf-token" content="<?php echo htmlspecialchars($_SESSION['csrf_token']); ?>">
+</head>
+<body class="admin-layout">
