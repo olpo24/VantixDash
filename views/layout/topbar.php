454dd5df9b3a692026465df418184e5259e6de5a
Ole Poetter <48469242+olpo24@users.noreply.github.com>
2026-01-21 07:13:58 +0100

    Update version display to use SettingsService

--- views/layout/topbar.php
+++ views/layout/topbar.php
@@ -1,8 +1,17 @@
+<?php
+use VantixDash\Config\SettingsService;
+
+if (!isset($settingsService)) {
+    $settingsService = new SettingsService($configService);
+}
+
+$currentVersion = $settingsService->getVersion();
+?>
 <header class="topbar">
     <button id="sidebar-toggle" class="sidebar-toggle">
         <i class="ph ph-list"></i>
     </button>
     <div class="topbar-info">
-        <span>VantixDash v<?php echo htmlspecialchars((string)$configService->getVersion()); ?></span>
+        <span>VantixDash v<?php echo htmlspecialchars($currentVersion); ?></span>
     </div>
 </header>
